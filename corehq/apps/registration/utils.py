32d4a831c82139a1e86583cc60f335ff4f6225b4
Spencer <stank@dimagi.com>
2012-08-09 16:37:51 -0400

    new name for projects in an organization is org_name:domain_name

--- corehq/apps/registration/utils.py
+++ corehq/apps/registration/utils.py
@@ -42,8 +42,8 @@ def request_new_domain(request, form, org, new_user=True):
         dom_req.activation_guid = uuid.uuid1().hex
 
     if org:
-        new_domain = Domain(slug=form.cleaned_data['domain_name'],
-                            is_active=False,
+        new_domain = Domain(name=org + '-' + form.cleaned_data['domain_name'],
+                            is_active=False, slug = form.cleaned_data['domain_name'],
                             date_created=datetime.utcnow(), organization=org)
     else:
         new_domain = Domain(name=form.cleaned_data['domain_name'],
